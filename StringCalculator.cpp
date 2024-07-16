0f813a4c2308bb1b85c9c724092ea478313b2ae7
nitish1703-sys <nitish.tanguturi@gmail.com>
2024-07-16 08:18:24 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,1 +1,65 @@
+#include <gtest/gtest.h>
+#include "StringCalculator.h"
 
+TEST(StringCalculatorAddTests, ExpectZeroForEmptyInput) {
+    int expectedresult = 0;
+    const char* input = "";
+    int result = add(input);
+    ASSERT_EQ(result, expectedresult);
+}
+
+TEST(StringCalculatorAddTests, ExpectZeroForSingleZero) {
+    int expectedresult = 0;
+    const char* input = "0";
+    int result = add(input);
+    ASSERT_EQ(result, expectedresult);
+}
+
+TEST(StringCalculatorAddTests, ExpectSumForTwoNumbers) {
+    int expectedresult = 3;
+    const char* input = "1,2";
+    int result = add(input);
+    ASSERT_EQ(result, expectedresult);
+}
+
+TEST(StringCalculatorAddTests, ExpectSumWithNewlineDelimiter) {
+    int expectedresult = 6;
+    const char* input = "1\n2,3";
+    int result = add(input);
+    ASSERT_EQ(result, expectedresult);
+}
+
+TEST(StringCalculatorAddTests, IgnoreNumbersGreaterThan1000) {
+    int expectedresult = 1;
+    const char* input = "1,1001";
+    int result = add(input);
+    ASSERT_EQ(result, expectedresult);
+}
+
+TEST(StringCalculatorAddTests, ExpectSumWithCustomDelimiter) {
+    int expectedresult = 3;
+    const char* input = "//;\n1;2";
+    int result = add(input);
+    ASSERT_EQ(result, expectedresult);
+}
+
+TEST(StringCalculatorHasCustomDelimiterTests, CustomDelimiterNotPresent) {
+    int expectedresult = 6;
+    const char* input = "1,2,3";
+    int result = add(input);
+    ASSERT_EQ(result, expectedresult);
+}
+TEST(StringCalculatorAddTests, ExpectSumWithCustomDelimiter_EmptyString) {
+    int expectedresult = 0;
+    const char* input = "";
+    int result = add(input);
+    ASSERT_EQ(result, expectedresult);
+}
+TEST(StringCalculatorAddTests, HandleNegativeNumbers) {
+    const char* input = "-1,2";
+    testing::internal::CaptureStdout();
+    int result = add(input);
+    std::string output = testing::internal::GetCapturedStdout();
+    ASSERT_EQ(result, -1);
+    ASSERT_EQ(output, "Negatives not allowed: -1 \n");
+}
